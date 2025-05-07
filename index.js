ad1fd34de5c854de7686d29a956a9f3d8f103887
isaiah973 <adeolaisaiah01@gmail.com>
2025-05-06 22:01:20 -0400

    added counter

---
+++ index.js
@@ -0,0 +1,17 @@
+    // let name = "Adeola Isaiah";
+    // let city = "Ibadan";
+    // let myFavoriteColor = "Black";//Camel case
+    // let best_food = "Rice";//Snake Case
+    // const pi = 3.142;
+    
+    const digit = document.querySelector('.digit');
+    
+
+    let count = 0;
+    const addBtn = document.querySelector('.increase-btn').addEventListener('click',  function(){
+        digit.textContent = count++;
+    });
+    const minusBtn = document.querySelector('.decrease-btn').addEventListener('click', function(){
+        digit.textContent = count--;  
+    });
+  
